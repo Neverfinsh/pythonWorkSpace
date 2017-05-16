6849658ff95f10ef8276c8ca17cfb122cf9469db
Neverfinsh <947395491@qq.com>
2017-05-16 16:50:58 +0800

    简单的python语法

---
+++ Python_01.py
@@ -0,0 +1,23 @@
+ # print("hello world ");
+# name =input ();
+# print ("输入程序");
+# print(name);
+
+
+
+# print方法的可以执行运算的方法
+# print(1+2);
+
+
+#python基础  括号 不需要用冒号同时if（）不用冒号；
+a=10
+if  a>19:
+   print(a)
+else :
+      print(-a)
+
+
+
+
+
+
